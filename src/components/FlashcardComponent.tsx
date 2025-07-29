77b125887c784543eac8f0263a8b6a309b241db8
aalizelau <aalizelau@gmail.com>
2025-07-29 12:00:20 -0700

    centralize sample data

--- src/components/FlashcardComponent.tsx
+++ src/components/FlashcardComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
-import { Flashcard } from './FlashcardApp';
+import { Flashcard } from '@/data/flashcards';
 import { RotateCcw } from 'lucide-react';
 
 interface FlashcardComponentProps {
