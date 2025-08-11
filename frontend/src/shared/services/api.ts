24954c5352bd6601ce2681a36ed92b3ab235f622
Aalizelau <aalizelau@gmail.com>
2025-08-10 22:49:22 -0700

    fix: add variable to point to API

--- frontend/src/shared/services/api.ts
+++ frontend/src/shared/services/api.ts
@@ -13,7 +13,7 @@ import {
 } from '@/shared/types/api';
 import { auth } from '@/features/auth/contexts/AuthContext';
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = import.meta.env.VITE_API_ADDRESS;
 
 class ApiClient {
   private async request<T>(
